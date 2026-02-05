105651e3029974be402f5794c738566d3a08d7c6
Bugen Zhao <i@bugenzhao.com>
2026-02-05 17:07:19 +0800

    bump prost/tonic to 0.14
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let derive_serde = "#[derive(serde::Serialize, serde::Deserialize, datasize::DataSize)]";
+    let derive = "#[derive(serde::Serialize, serde::Deserialize, datasize::DataSize)]";
 
-    tonic_build::configure()
-        .message_attribute(".", derive_serde)
-        .enum_attribute(".", derive_serde)
+    tonic_prost_build::configure()
+        .build_transport(false)
+        .build_client(false)
+        .message_attribute(".", derive)
+        .enum_attribute(".", derive)
         .compile_protos(
             &["proto/opentelemetry/proto/collector/trace/v1/trace_service.proto"],
             &["proto/"],
